cef977574dff2987b9d74a87937d656ff57cc342
reyregio <marisafenogliomnf@gmail.com>
2023-03-07 13:41:51 -0300

    final
    detalles finales

--- app.js
+++ app.js
@@ -1,37 +1,27 @@
-class RandomUser{
-    constructor(){}
+class RandomUser {
+    constructor() {}
     
-    static fetchFromAPI(){
-        let URL ='https://randomuser.me/api/';
+    static fetchFromAPI() {
+        let URL = "https://randomuser.me/api/";
         fetch(URL)
-        .then(response =>response.json())
-        .then(data => RandomUser.renderUserData(data))
-        .catch(error=>alert(error));
+        .then((response) => response.json())
+        .then((data) => RandomUser.renderUserData(data))
+        .catch((error) => alert(error));
     }
-    static renderUserData(data){
-        let user =data.results[0];
-        let cardElem=document.querySelector('.card');
-        cardElem.innerHTML=` <div class="card-head">
+    
+    static renderUserData(data) {
+        let user = data.results[0];
+        let cardElem = document.querySelector(".card");
+        cardElem.innerHTML = ` <div class="card-head">
         
         <div class="user-image">
         <img src="${user.picture.large}" alt="">
         </div>
         </div>
         <div class="card-body">
-        <div class="user-post-adress">
-        <div>
-        <span>${user.location.street.number}</span> <span>Numero</span>
-        </div>
-        <div>
-        <span>${user.location.postcode}</span> <span>Código Postal</span>
-        </div>
-        <div>
-        <span>${user.location.street.name}</span><span>Calle</span>
-        </div>
-        </div>
+        
         
         <div class="user-name">
-        <span class="user-name-title">${user.name.title}.</span>
         <span class="user-name-full">${user.name.first} ${user.name.last},</span>
         <span class="user-age">${user.dob.age}</span>
         </div>
@@ -43,16 +33,59 @@ class RandomUser{
         <div class="card-foot">
         <div class="user-contact-info">
         
-        <span> <i class="fas fa-envelope"> </i> ${user.email} <button class="copy"><i class="fa-regular fa-copy"></i></button></span>
-        <span> <i class="fa-solid fa-mobile-button"> </i> ${user.cell} <button class="copy"><i class="fa-regular fa-copy"></i></button></span>
+        <span class="mail"> <i class="fas fa-envelope"> </i>  ${
+            user.email
+        }<button class="copy" data-clipboard-text="${
+            user.email
+        }"><i class="fa-regular fa-copy"></i></button></span>
+        <span> <i class="fa-solid fa-mobile-button"> </i> ${
+            user.cell
+        } <button class="copy" data-clipboard-text="${
+            user.cell
+        }"><i class="fa-regular fa-copy"></i></button></span>
         </div>
-        </div>`
-    }    
-}
-
-RandomUser.fetchFromAPI();
-
-let btn =document.querySelector("button");
-btn.onclick=() =>{
-    window.print();
-}
+        </div>`;
+        
+        // Add event listener to copy buttons
+        const copyButtons = document.querySelectorAll(".copy");
+        copyButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                const textToCopy = button.dataset.clipboardText;
+                navigator.clipboard.writeText(textToCopy).then(
+                    () => {
+                        console.log("Text copied to clipboard");
+                    },
+                    (err) => {
+                        console.error("Could not copy text: ", err);
+                    }
+                    );
+                });
+            });
+        }
+    }
+    
+    RandomUser.fetchFromAPI();
+    
+    
+    let copy=document.querySelectorAll(".mail");
+    
+    copy.forEach(text=>{
+        const btnCopy = text.querySelector('.copy');
+        const content = text.querySelector('span:first-child');
+        
+        btnCopy.addEventListener('click',() =>{
+            navigator.clipboard.writeText(content.textContent)
+            btnCopy.textContent = 'Copied!'
+        })
+    })
+    
+    // Add event listener to print button
+    document.getElementById("print-button").addEventListener("click", function() {
+        // Hide print button before printing
+        this.style.display = "none";
+        // Call print function
+        window.print();
+        // Show print button after printing
+        this.style.display = "block";
+    });
+    
