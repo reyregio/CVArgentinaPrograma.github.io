66a40f028285299f673714be695b10294e556e87
reyregio <71472871+reyregio@users.noreply.github.com>
2023-06-15 13:43:34 -0300

    Update and rename app.js to script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('resize', function() {
+  var mapContainer = document.getElementById('map-container');
+  var iframe = mapContainer.querySelector('iframe');
+  iframe.style.height = mapContainer.offsetWidth * 0.5625 + 'px'; /* 16:9 aspect ratio (change to your needs) */
+});
+
+window.dispatchEvent(new Event('resize'));
+    
