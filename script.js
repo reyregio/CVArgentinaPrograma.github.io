332241c052eb5dd73187d14d8cf47fc6c1f3f6b8
reyregio <marisafenogliomnf@gmail.com>
2023-09-27 02:39:32 -0300

    como sea

--- script.js
+++ script.js
@@ -15,5 +15,16 @@ if(navToggle){
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
-        })
-        }
+    })
+}
+
+
+/*===========================REMOVE MENU MOBILE========================= */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
