0c6015cb5fb1df90eade273c01b0be6091e8e6c6
reyregio <marisafenogliomnf@gmail.com>
2023-10-08 10:50:52 -0300

    scroll-down

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 // ========================== SHOW MENU ===================
-const navMenu=document.getElementById('nav-menu'),
-navToggle = document.getElementById('nav-toggle'),
-navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+navToggle = document.getElementById("nav-toggle"),
+navClose = document.getElementById("nav-close");
 
 // ==========MENU SHOW=================
-// Validate if constant exists 
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
-        navMenu.classList.add("show-menu")
-    })  
+// Validate if constant exists
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
 }
 
 // ==========MENU HIDDEN=================
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    })
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
 }
-
 
 /*===========================REMOVE MENU MOBILE========================= */
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+};
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=========================== CHANGE BACKGROUND HEADER ========================= */
+const shadowHeader = () => {
+    const header = document.getElementById("header");
+    
+    this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+
+window.addEventListener('scroll', shadowHeader); 
