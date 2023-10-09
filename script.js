ed5167a1c0aea19027b25313e32e41b257574f09
reyregio <marisafenogliomnf@gmail.com>
2023-10-09 19:14:14 -0300

    emial

--- script.js
+++ script.js
@@ -32,9 +32,40 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 const shadowHeader = () => {
     const header = document.getElementById("header");
     
-    this.scrollY >= 50
-    ? header.classList.add("shadow-header")
-    : header.classList.remove("shadow-header");
+    if (window.scrollY >= 50) {
+        header.classList.add("shadow-header");
+    } else {
+        header.classList.remove("shadow-header");
+    }
 };
 
-window.addEventListener('scroll', shadowHeader); 
+window.addEventListener("scroll", shadowHeader);
+
+
+/*===========================Email js======================== */
+const contactForm = document.getElementById("contact-form"),
+contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+    e.preventDefault();
+    
+    //serviceI-templateÇI form- pùblickey
+    emailjs.sendForm('service_day3w4d', 'template_g9or6od', '#contact-form', 'SIOObORQn9rPWgtdq')
+    .then(() => {
+        //show sent message
+        contactMessage.textContent = 'Message sent successfully ✅';
+        
+        //remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = '';
+        }, 5000);
+        
+        //clear inputs fields
+        contactForm.reset();
+    }, () => {
+        //show error
+        contactMessage.textContent = 'Message not sent (service error) ❌';
+    });
+}
+
+contactForm.addEventListener('submit', sendEmail);
