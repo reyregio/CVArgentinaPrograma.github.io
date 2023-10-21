37f13f3b400e663a6a680f10c236431b009ba49f
reyregio <marisafenogliomnf@gmail.com>
2023-10-20 22:21:52 -0300

    cositas

--- script.js
+++ script.js
@@ -107,3 +107,42 @@ const sendEmail = (e) => {
             });
         }
         
+        // ======================== DARK LIGTH THEME ============================
+        const themeButton = document.getElementById('theme-button')
+        const darkTheme = 'dark-theme'
+        const iconTheme= 'ri-sun-line'
+        
+        const selectedTheme = localStorage.getItem('selected-theme')
+        const selectedIcon = localStorage.getItem('selected-icon')
+        
+        const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+        const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+        
+        if(selectedTheme){
+            document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+            themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+        }
+        
+        themeButton.addEventListener('click',()=>{
+            document.body.classList.toggle(darkTheme)
+            themeButton.classList.toggle(iconTheme)
+            localStorage.setItem('selected-theme',getCurrentTheme())
+            localStorage.setItem('selected-icon',getCurrentIcon())
+        })
+        
+        // SCROLL REVEAL ANIMATION
+        
+        const sr =scrollReveal({
+            origin: 'top',
+            distance:'60px',
+            duration:2500,
+            delay:400,
+        })
+        
+        sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin:'right'})
+        sr.reveal(`.home__name, .home__info,
+        .about__container, .section__title-1, .about__info,
+        .contact__social, .contact__data`,  {origin:'left'})
+        
+        sr.reveal(`.services__card , .projects__card`, {interval:100})
+        
